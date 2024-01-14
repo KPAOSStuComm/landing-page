59f066d5aae8612956e9d734f3c722152385e6a0
Huto <92351427+Hutotpn@users.noreply.github.com>
2024-01-14 13:35:56 +0000

    Change to KPAOS Open House, Children's Day and Teachers' Day

--- landing.js
+++ landing.js
@@ -1,19 +1,31 @@
 window.onload = function () {
   document.getElementById("landing").innerHTML =
-    '<img src="" alt="สวัสดีปีใหม่ 2567 โรงเรียนองค์การบริหารส่วนจังหวัดกระบี่" id="landing-img" />';
+    '<img src="" alt="" id="landing-img" />';
 };
 
-setInterval(() => {
-  // Select the image element
-  let imgElement = document.getElementById("landing-img");
+// Select the image element
+let imgElement = document.getElementById("landing-img");
 
-  // Check if the viewport is less than 900px
-  if (window.matchMedia("(max-width: 900px)").matches) {
-    // If true, change the source of the image
+setInterval(() => {
+  setTimeout(() => {
     imgElement.src =
-      "//kpaosstucomm-proj-landing.netlify.app/images/HNY-2024-KPAOS-compact.jpg";
-  } else {
+      "//kpaosstucomm-proj-landing.netlify.app/images/open-house-10.jpg";
+
+    imgElement.alt =
+      "กิ่งจันวิชาการ ครั้งที่ 10 ณ โรงเรียนองค์การบริหารส่วนจังหวัดกระบี่";
+  }, 2000);
+
+  setTimeout(() => {
     imgElement.src =
-      "//kpaosstucomm-proj-landing.netlify.app/images/HNY-2024-KPAOS-wide.jpg";
-  }
-}, 1000);
+      "//kpaosstucomm-proj-landing.netlify.app/images/kids-day-2024.jpg";
+
+    imgElement.alt = "วันเด็กแห่งชาติ, 13 มกราคม 2567";
+  }, 2000);
+
+  setTimeout(() => {
+    imgElement.src =
+      "//kpaosstucomm-proj-landing.netlify.app/images/teachers-day-2024.jpg";
+
+    imgElement.alt = "วันครู, 16 มกราคม 2567";
+  }, 2000);
+}, 6000);
