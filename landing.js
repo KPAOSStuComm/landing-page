736e4c734904f0e1f016579e8c601ab2f66f2ddd
Huto <92351427+Hutotpn@users.noreply.github.com>
2024-01-14 14:05:31 +0000

    New set

--- landing.js
+++ landing.js
@@ -5,27 +5,26 @@ window.onload = function () {
   // Select the image element
   let imgElement = document.getElementById("landing-img");
 
+  let images = [
+    {
+      src: "//kpaosstucomm-proj-landing.netlify.app/images/open-house-10.jpg",
+      alt: "กิ่งจันวิชาการ ครั้งที่ 10 ณ โรงเรียนองค์การบริหารส่วนจังหวัดกระบี่",
+    },
+    {
+      src: "//kpaosstucomm-proj-landing.netlify.app/images/kids-day-2024.jpg",
+      alt: "วันเด็กแห่งชาติ, 13 มกราคม 2567",
+    },
+    {
+      src: "//kpaosstucomm-proj-landing.netlify.app/images/teachers-day-2024.png",
+      alt: "วันครู, 16 มกราคม 2567",
+    },
+  ];
+
+  let index = 0;
+
   setInterval(() => {
-    setTimeout(() => {
-      imgElement.src =
-        "//kpaosstucomm-proj-landing.netlify.app/images/open-house-10.jpg";
-
-      imgElement.alt =
-        "กิ่งจันวิชาการ ครั้งที่ 10 ณ โรงเรียนองค์การบริหารส่วนจังหวัดกระบี่";
-    }, 2000);
-
-    setTimeout(() => {
-      imgElement.src =
-        "//kpaosstucomm-proj-landing.netlify.app/images/kids-day-2024.jpg";
-
-      imgElement.alt = "วันเด็กแห่งชาติ, 13 มกราคม 2567";
-    }, 4000);
-
-    setTimeout(() => {
-      imgElement.src =
-        "//kpaosstucomm-proj-landing.netlify.app/images/teachers-day-2024.png";
-
-      imgElement.alt = "วันครู, 16 มกราคม 2567";
-    }, 6000);
-  }, 8000);
+    imgElement.src = images[index].src;
+    imgElement.alt = images[index].alt;
+    index = (index + 1) % images.length;
+  }, 2000);
 };
